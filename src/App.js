41517f9d75409823698eaf9723ef1375b5071685
Vijay Kumar V <59360964+ViNu-23@users.noreply.github.com>
2023-06-03 15:00:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,15 +33,15 @@ function App() {
       <h1>Card Color Picker</h1>
 
       <div className='first'>
-        <p className='cards' id='lightBlue' style={{ backgroundColor: '#2EC4B6', width: '100px', height: '100px' }} onClick={handleClick}></p>
-        <p className='cards' id='darkGrey' style={{ backgroundColor: '#0A1128', width: '100px', height: '100px' }} onClick={handleClick}></p>
-        <p className='cards' id='red' style={{ backgroundColor: '#FF0000', width: '132px', height: '132px' }} onClick={handleClick}></p>
-        <p className='cards' id='lightYellow' style={{ backgroundColor: '#CCFF66', width: '100px', height: '100px' }} onClick={handleClick}></p>
-        <p className='cards' id='lightGrey' style={{ backgroundColor: '#605856', width: '100px', height: '100px' }} onClick={handleClick}></p>
-        <p className='cards' id='darkBlue' style={{ backgroundColor: '#5D2E8C', width: '100px', height: '100px' }} onClick={handleClick}></p>
-        <p className='cards' id='ash' style={{ backgroundColor: '#899878', width: '100px', height: '100px' }} onClick={handleClick}></p>
-        <p className='cards' id='lightOrange' style={{ backgroundColor: '#FED18C', width: '100px', height: '100px' }} onClick={handleClick}></p>
-        <p className='cards' id='lightPink' style={{ backgroundColor: '#DB7F8E', width: '100px', height: '100px' }} onClick={handleClick}></p>
+        <p className='cards' id='lightBlue' style={{ backgroundColor: '#2EC4B6', width: '80px', height: '100px' }} onClick={handleClick}></p>
+        <p className='cards' id='darkGrey' style={{ backgroundColor: '#ff00ee', width: '80px', height: '100px' }} onClick={handleClick}></p>
+        <p className='cards' id='red' style={{ backgroundColor: '#b500ff', width: '132px', height: '132px' }} onClick={handleClick}></p>
+        <p className='cards' id='lightYellow' style={{ backgroundColor: '#8000ff', width: '80px', height: '100px' }} onClick={handleClick}></p>
+        <p className='cards' id='lightGrey' style={{ backgroundColor: '#6900ff', width: '80px', height: '100px' }} onClick={handleClick}></p>
+        <p className='cards' id='darkBlue' style={{ backgroundColor: '#000aff', width: '80px', height: '100px' }} onClick={handleClick}></p>
+        <p className='cards' id='ash' style={{ backgroundColor: '#006eff', width: '80px', height: '100px' }} onClick={handleClick}></p>
+        <p className='cards' id='lightOrange' style={{ backgroundColor: '#00d1ff', width: '80px', height: '100px' }} onClick={handleClick}></p>
+        <p className='cards' id='lightPink' style={{ backgroundColor: '#00ffb3', width: '80px', height: '100px' }} onClick={handleClick}></p>
       </div>
 
       <div className='second'>
@@ -50,8 +50,8 @@ function App() {
           <p className='sfName' >{input}</p>
         </div>
         <div className='sSecond'>
-          <p className='line1' >Design my Card</p>
-          <p className='line2' >Hey there User! Want to build your personalised card? Choose a colour from the palette above and fill your name</p>
+          <p className='line1' >Design Card</p>
+          
           <p className='line3' >Your selected colour is <b>{color}</b></p>
           <p className='line4' >Name</p>
           <input className='input1' name='name' value={input} onChange={handleChange} ></input>
